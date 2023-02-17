8dbea42dfa28fabc90c3cc887c20238b7c256b07
yuta10200831 <124555669+yuta10200831@users.noreply.github.com>
2023-02-17 19:37:00 +0900

    Revert "Isue4~6"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,11 +15,10 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:plan, :date)
+    params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_week 
-
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -35,10 +34,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :wday=> wdays[(@todays_date + x).wday], :plans => today_plans }
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
+
   end
 end
